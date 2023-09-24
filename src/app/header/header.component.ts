664fc1dbafed16e129a198603fb5ceebf4edf205
El hadji Rawane Meissa Sow <96692209+RawaneG@users.noreply.github.com>
2023-09-24 04:11:47 +0000

    UI Optimization

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,9 @@
-import { HttpClientService } from '../services.service';
-import { IndexDBService } from '../index-db.service';
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
-import { Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { AuthService } from '../auth.service';
+import { Component, OnInit } from '@angular/core';
+import { IndexDBService } from '../index-db.service';
+import { HttpClientService } from '../services.service';
 
 @Component({
   selector: 'app-header',
@@ -11,41 +11,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  currentUser: any = {};
-  currentSeller: any;
+  monRole: any;
+  id: number = 0;
   shops: any[] = [];
   currentStore: any;
   boutiquierId: any;
-  id: number = 0;
-  monRole: any;
+  currentSeller: any;
+  currentUser: any = {};
+  cartIcon : boolean = false;
 
   constructor(
-    private httpService: HttpClientService,
-    private indexDBService: IndexDBService,
-    private service: AuthService,
-    public location: Location,
     public route: Router,
+    public location: Location,
+    private service: AuthService,
+    private indexDBService: IndexDBService,
+    private httpService: HttpClientService,
   ) { }
 
-  translate()
-  {
-    let nav = document.querySelector('#nav-bar');
-    nav?.classList.toggle('translate');
-  }
-  openPanier()
-  {
-    document.querySelector('.overlay')?.classList.remove('hide_parent');
-    document.querySelector('.second-parent')?.classList.remove('hide_parent');
-  }
-  refresh(): void
-  {
-    let currentUrl = this.route.url;
-    this.route.navigateByUrl('/', { skipLocationChange: true }).then(() => this.route.navigate([currentUrl]));
-  }
+  deconnexion() { this.service.deconnecter(); }
+  close() { document.querySelector('.popup-container')?.classList.add('hidden'); }
+  open() { document.querySelector('.popup-container')?.classList.remove('hidden'); }
+  showMyCart() { this.route.url === "/poc"? this.cartIcon = true : this.cartIcon = false; }
+  translate() { let nav = document.querySelector('#nav-bar'); nav?.classList.toggle('translate'); }
+  refresh(): void { let currentUrl = this.route.url; this.route.navigateByUrl('/', { skipLocationChange: true }).then(() => this.route.navigate([currentUrl])); }
+  openPanier() { document.querySelector('.overlay')?.classList.remove('hide_parent'); document.querySelector('.second-parent')?.classList.remove('hide_parent'); }
+
   switch(shop: any)
   {
-    this.indexDBService.clearData('currentShop');
     this.indexDBService.clearData('panier');
+    this.indexDBService.clearData('currentShop');
     this.indexDBService.putData({ id: this.id, boutique: shop }, 'currentShop').subscribe(
       {
         next: () => this.httpService.openSnackBar(shop.nomBoutique + ' a été choisie avec succès'),
@@ -53,21 +47,11 @@ export class HeaderComponent implements OnInit {
         complete: () => console.log('Complété avec succès')
       }
     );
-  }
-  open()
-  {
-    document.querySelector('.popup-container')?.classList.remove('hidden');
-  }
-  close()
-  {
-    document.querySelector('.popup-container')?.classList.add('hidden');
-  }
-  deconnexion()
-  {
-    this.service.deconnecter();
+    this.currentStore = shop;
   }
   ngOnInit(): void
   {
+    this.showMyCart();
     this.indexDBService.getData('currentUser').subscribe(
       data =>
       {
@@ -77,5 +61,6 @@ export class HeaderComponent implements OnInit {
       },
       error => console.log("Vous n'avez pas encore d'utilisateur " + error)
     );
+    this.indexDBService.getData('currentShop').subscribe((data : any) => this.currentStore = data[0].boutique)
   }
 }
